ec17922d023003c2e4193ea42ed581f5b6c8fb50
Steven Johnston <sjohnston@atomic.vc>
2024-02-23 03:17:01 -0500

    Re-install postcss for tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './app/**/*.{js,ts,jsx,tsx,mdx,.ttf}',
+        './app/**/*.{js,ts,jsx,tsx,mdx,ttf}',
     ],
     theme: {
         extend: {},
