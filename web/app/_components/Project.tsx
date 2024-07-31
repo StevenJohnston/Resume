9fd65338de8119f5dba245dce5f0e162c0443493
Steven Johnston <sjohnston@atomic.vc>
2024-07-31 17:28:17 -0400

    Slightly more mobile friendly

--- web/app/_components/Project.tsx
+++ web/app/_components/Project.tsx
@@ -14,13 +14,13 @@ export default function Project({
   return (
     <div className={`${tight ? 'mb-1' : 'mb-2'}`}>
       <div className={`flex flex-col sm:flex-row sm:justify-between ${tight ? 'mb-1' : 'mb-2'} sm:items-end`}>
-        <div className="text-base font-bold leading-none inline">
+        <div className="text-base font-bold leading-none inline-flex flex-wrap">
           <span>
             {title}
           </span>
           &nbsp;
           {
-            link && <a href={link} className="underline text-blue-600 hover:text-blue-800 text-xs">{link.split('https://')[1]}</a>
+            link && <a href={link} className="underline text-blue-600 hover:text-blue-800 text-xs overflow-ellipsis max-w-full overflow-hidden">{link.split('https://')[1]}</a>
           }
         </div>
       </div>
